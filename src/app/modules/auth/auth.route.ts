4d03002d30c57629274136b043a70fd7e897b79a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-06-26 08:35:19 +0600

    [UPDATED] auth route

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserValidation } from '../user/user.validation';
 import { AuthController } from './auth.controller';
+import {AuthValidation} from "./auth.validation";
 
 const router = express.Router();
 
@@ -10,5 +11,18 @@ router.post(
   validateRequest(UserValidation.createUserZodSchema),
   AuthController.createUser
 );
+
+router.post(
+    '/login',
+    validateRequest(AuthValidation.loginZodSchema),
+    AuthController.createUser
+);
+
+router.post(
+    '/refresh-token',
+    validateRequest(AuthValidation.refreshTokenZodSchema),
+    AuthController.refreshToken
+);
+
 
 export const AuthRoutes = router;
